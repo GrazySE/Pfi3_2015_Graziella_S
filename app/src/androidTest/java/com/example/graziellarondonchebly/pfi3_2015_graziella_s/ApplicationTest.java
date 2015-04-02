7c37ab2ab90a8e7ca930980a0581c4fd729a1299
GrazySE <batmansflicka@gmail.com>
2015-04-02 09:53:10 +0200

    Initial commit

---
+++ app/src/androidTest/java/com/example/graziellarondonchebly/pfi3_2015_graziella_s/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.graziellarondonchebly.pfi3_2015_graziella_s;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
