8fc009dd62b0488bcbf0096fe54112898b9ec925
GrazySE <batmansflicka@gmail.com>
2015-04-28 23:46:32 +0200

    Fungerande Uppgift 4

---
+++ assignment_4/src/main/java/com/example/graziellarondonchebly/assignment4/Planet.java
@@ -0,0 +1,73 @@
+package com.example.graziellarondonchebly.assignment4;
+
+/**
+ * Created by graziellarondonchebly on 15-04-28.
+ */
+import android.graphics.drawable.Drawable;
+
+import java.io.Serializable;
+
+
+public class Planet implements Serializable {
+    String title;
+    String radius;
+    String temp;
+    Drawable image;
+    String summary;
+
+    public Planet(String title, String radius, String temp, Drawable image, String summary) {
+        this.title = title;
+        this.temp = temp;
+        this.image = image;
+        this.radius=radius;
+        this.summary = summary;
+
+    }
+
+    public String getTemp() {
+        return temp;
+    }
+
+    public void setTemp(String temp) {
+        this.temp = temp;
+    }
+
+    public Drawable getImage() {
+        return image;
+    }
+
+    public void setImage(Drawable image) {
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getRadius() {
+        return radius;
+    }
+
+    public void setRadius(String radius) {
+        this.radius = radius;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return title;
+    }
+
+    public String getSummary() {
+        return summary;}
+
+    public void setSummary(String summary){
+        this.summary=summary;
+    }
+
+}
